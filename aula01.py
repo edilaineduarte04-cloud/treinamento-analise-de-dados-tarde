ae294dbb09aa5ef2655c32408bff864c4ac80539
edilaineduarte04-cloud <edilaine.duarte04@gmail.com>
2026-07-02 16:51:02 -0300

    feat:adiciona codigos

---
+++ aula01.py
@@ -0,0 +1,12 @@
+# O comando print exibe uma informação na tela do usuário
+#print(30) #Idade do usuário
+#print("30") # Idade do usuário
+
+idade = 30 # idade um
+idade_dois = "30" # idade dois
+
+print (idade)
+print (idade_dois)
+
+idade_futura = idade + 1
+print (idade_futura)
